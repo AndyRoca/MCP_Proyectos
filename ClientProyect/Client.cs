706f220a035b7ed9462cb0199e6354104b98171e
AndyRocaUwU <andres.rivera.geo@gmail.com>
2025-08-16 19:19:39 +0200

    Add initial project files for MCP client and server applications
    - Created .gitignore to exclude Visual Studio and build artifacts
    - Implemented MCP client with stdio transport and tool listing
    - Set up MCP server with logging and tool functionality
    - Defined project structure in solution and project files

---
+++ ClientProyect/Client.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using ModelContextProtocol.Client;
+using ModelContextProtocol.Protocol;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Configuration 
+    .AddEnvironmentVariables()
+    .AddUserSecrets<Program>();
+
+var clientTransport = new StdioClientTransport(new()
+{
+    Name = "Demo Server",
+    Command = "dotnet",
+    Arguments = new[]
+    {
+        @"C:\MCP_Proyectos\ConsoleApp1\bin\Debug\net8.0\McpServer.dll"
+    }
+});
+
+Console.WriteLine("Setting up stdio transport");
+
+await using var mcpClient = await McpClientFactory.CreateAsync(clientTransport);
+
+Console.WriteLine("Listing tools");
+
+var tools = await mcpClient.ListToolsAsync();
+
+foreach (var tool in tools)
+{
+    Console.WriteLine($"Connected to server with tools: {tool.Name}");
+}
+
+var result = await mcpClient.CallToolAsync(
+    "add",
+    new Dictionary<string, object?>() { ["a"] = 1, ["b"] = 3  },
+    cancellationToken:CancellationToken.None);
+
+Console.WriteLine("Result: " + ((TextContentBlock)result.Content[0]).Text);
